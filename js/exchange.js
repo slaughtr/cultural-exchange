7870abd0138172b07421c5f24261bf5fdb2faa07
Colin Bloom and Dallas Slaughter <colin@>
2017-04-19 14:38:00 -0700

    add json

--- js/exchange.js
+++ js/exchange.js
@@ -1,15 +1,12 @@
-const countryAbbreviation1 = require('./../js/website-interface.js').countryAbbreviation1;
-const countryAbbreviation2 = require('./../js/website-interface.js').countryAbbreviation2;
+function getExchange(countryAbbreviation1, countryAbbreviation2) {
 
-  function getExchange(country) {
-
-    $.get(`http://api.fixer.io/latest?base=${countryAbbreviation1}&symbols=${countryAbbreviation1},${countryAbbreviation2}`).then(function(response) {
-      $('#infoBox').text(response.rates.countryAbbreviation1);
-    }).fail(function(error) {
-      $('#infoBox').text('There was an error');
-    });
-  }
+  $.get('http://api.fixer.io/latest?base='+countryAbbreviation1+'&symbols='+countryAbbreviation1+','+countryAbbreviation2).then(function(response) {
+    let testVar = countryAbbreviation1
+    $('#infoBox').text('There are '+Object.values(response.rates)+' '+countryAbbreviation1+' to every '+countryAbbreviation2);
+  }).fail(function(error) {
+    $('#infoBox').text('There was an error');
+  });
+};
 
 
-
-exports.getExchange = getExchange();
+exports.exchangeModule = getExchange;
--- js/website-interface.js
+++ js/website-interface.js
@@ -1,27 +1,22 @@
-const getExchange = require('./../js/exchange.js').getExchange;
+const getExchange = require('./../js/exchange.js').exchangeModule;
 const getCurrencyAbbreviation = require('./../node_modules/country-currency-map').getCurrencyAbbreviation;
+
+var test = require('../js/exchange.js').test;
 
 let countryName1;
 let countryAbbreviation1;
 let countryName2;
 let countryAbbreviation2;
 
-
 $(function() {
-  $('#countrySearch').submit(function(event) {
+  $('#submitButton').click(function(event) {
     event.preventDefault();
+
     countryName1 = $('#country1Input').val();
-    countryAbbreviation1 = getCurrencyAbbreviation(`${countryName1}`);
+    countryAbbreviation1 = getCurrencyAbbreviation(countryName1);
     countryName2 = $('#country2Input').val();
-    countryAbbreviation2 = getCurrencyAbbreviation(`${countryName2}`);
+    countryAbbreviation2 = getCurrencyAbbreviation(countryName2);
 
-    console.log(countryName2);
-    console.log(countryName1);
-    console.log(countryAbbreviation2);
-    console.log(countryAbbreviation1);
-
+    getExchange(countryAbbreviation1, countryAbbreviation2);
   })
 });
-
-exports.countryAbbreviation1 = countryAbbreviation1;
-exports.countryAbbreviation2 = countryAbbreviation2;
